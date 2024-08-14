3e2fd1060946adc2d849a85816ed89666230e8e1
Taiwo Shabi <taiwo.shabi@moniepoint.com>
2024-08-14 15:08:38 +0100

    complete module 5
    Learnings: Check all titles in the module

---
+++ ecommerce_app/lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -0,0 +1,21 @@
+import 'package:ecommerce_app/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AccountScreenController extends StateNotifier<AsyncValue> {
+  AccountScreenController({required this.authRepository})
+      : super(const AsyncValue.data(null));
+  final FakeAuthRepository authRepository;
+
+  Future<void> signOut() async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(() => authRepository.signOut());
+  }
+}
+
+final accountScreenControllerProvider =
+    StateNotifierProvider.autoDispose<AccountScreenController, AsyncValue>(
+        (ref) {
+  final authRepository = ref.watch(authRepositoryProvider);
+  return AccountScreenController(authRepository: authRepository);
+});
